6b0730485ec3cc30c19c3b86feafebcb83bd6a95
joshua.ziebarth <joshua.ziebarth@cave.lan>
2026-08-03 21:35:24 +0000

    asdasd

--- build.py
+++ build.py
@@ -60,28 +60,20 @@ class BuildSystem:
 
         # self._get_dev_env()
 
+        # TODO create build/.cache/ 
+        # store env_x64.json and 86 and mock.obj there
+        # only build those once locally
+        # gitignore build or .cache
+        # maybe only store?:
+        # PATH
+        # INCLUDE
+        # LIB
+        # LIBPATH
         self._env['x64'] = self._get_dev_env("x64")
         # self._env['x86'] = self._get_dev_env("x86")
 
     def _get_dev_env(self, arch: Arch) -> dict[str, str]:
         print("Generating build environment...")
-        # vsdevcmd = r"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-
-        # cmd = f'call "{vsdevcmd}" -arch={arch} && set'
-        # # print(repr(cmd))
-        # # ['cmd', '/c', cmd]
-        # try:
-        #     result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
-        # except subprocess.CalledProcessError as err:
-        #     print(f"Return code: {err.returncode}")
-        #     print("----- STDOUT -----")
-        #     print(err.stdout)
-        #     print("----- STDERR -----")
-        #     print(err.stderr)
-        #     sys.exit(1)
-
-
-        # env = os.environ.copy()
 
         launch = r"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1"
 
@@ -97,14 +89,12 @@ class BuildSystem:
             check=True
         )
 
-        env = {}
+        env = {} # os.environ.copy()
 
         for line in result.stdout.splitlines():
             if '=' in line:
                 k, v = line.split('=', 1)
                 env[k.upper()] = v
-
-        # print(env)
 
         return env
 
@@ -239,7 +229,7 @@ class BuildSystem:
         build_root.mkdir(parents=True, exist_ok=True)
         output_root.mkdir(parents=True, exist_ok=True)
         object_dir.mkdir(parents=True, exist_ok=True)
-        
+        # TODO only make relevent dirs?
         debug_build_path.mkdir(parents=True, exist_ok=True)
 
         includes.append(project)
@@ -260,9 +250,6 @@ class BuildSystem:
         except FileNotFoundError as err:
             return str(err)
 
-        # print (result)
-        # sys.exit(0)
-
         try:
             link_cmd = self._get_link_cmd(
                 proj_name.casefold(),
@@ -272,8 +259,6 @@ class BuildSystem:
             )
         except FileNotFoundError as err:
             return str(err)
-
-        print(link_cmd)
 
         # link
         try:
@@ -289,254 +274,6 @@ class BuildSystem:
             return str(err.output)
 
 
-
-        
-
-# def get_dev_env(self, arch: Literal['amd64', 'x86'] = 'amd64') -> dict[str, str]:
-#         cmd: str = (
-#             rf'"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" '
-#             rf'-arch={arch} && set'
-#         )
-
-#         result = subprocess.run(['cmd', '/c', cmd], capture_output=True, text=True, check=True)
-
-#         env = os.environ.copy()
-
-#         for line in result.stdout.splitlines():
-#             if '=' in line:
-#                 k, v = line.split('=', 1)
-#                 env[k] = v
-
-#         return env
-
-# def get_bof_path(bof_name: str) -> Path:
-#     bof_name = bof_name.casefold()
-
-#     for entry in BOFS_DIR.iterdir():
-#         if entry.is_dir() and entry.name.casefold() == bof_name:
-#             return entry
-
-#     raise FileNotFoundError(f"'{bof_name}' not found in '{BOFS_DIR}'")
-
-# def get_source_files(bof_dir: Path) -> list[Path]:
-#     sources: list[Path] = []
-
-#     for file in bof_dir.iterdir():
-#         if not file.is_file():
-#             continue
-
-#         if file.suffix.lower() in (".c", ".cpp"):
-#             sources.append(file)
-
-#     if not sources:
-#         raise FileNotFoundError(f"No C/C++ sources found in {bof_dir}")
-
-#     return sources
-
-# def get_compile_cmd(bof_dir:Path, arch:Literal['x64', 'x86'], debug:bool) -> list[str]:
-
-#     sources = get_source_files(bof_dir).copy()
-#     compile_sources = sources.copy()
-
-#     output_root: Path = BUILD_DIR / (DEBUG if debug else RELEASE) / arch
-#     obj_dir: Path = output_root / bof_dir.name
-
-#     obj_dir.mkdir(parents=True, exist_ok=True)
-
-#     cmd: list[str] = [
-#         "cl",
-#         "/nologo",
-#         "/c",
-#         "/GS-",
-#         "/EHsc",
-#         "/std:c++20",
-#         f"/I{COMMON_DIR}",
-#         f"/I{BOFS_DIR / bof_dir.name}",
-#         f"/Fo{obj_dir}\\",
-#         f"/Fd{obj_dir}\\"
-#     ]
-
-#     if debug:
-#         cmd += [
-#             "/D_DEBUG",
-#             "/Zi",
-#             "/MTd"
-#         ]
-#         compile_sources.append(Path(r"common\base\mock.cpp"))
-#     else:
-#         cmd += [
-#             "/Od",
-#             "/W0",
-#             "/MT"
-#         ]
-
-#     cmd += [str(x) for x in compile_sources]
-
-#     return cmd
-
-# def get_link_cmd(obj_dir: Path, output: Path, debug: bool) -> list[str]:
-
-#     obj_files = list(obj_dir.glob("*.obj"))
-
-#     if not obj_files:
-#         raise FileNotFoundError(f"No object files found in {obj_dir}")
-
-#     if debug:
-#         cmd = [
-#             "link",
-#             "/DEBUG",
-#             f"/PDB:{output.with_suffix('.pdb')}",
-#             f"/OUT:{output}"
-#         ]
-
-#         cmd += [str(x) for x in obj_files]
-#     else:
-#         cmd = [
-#             "link",
-#             "/lib",
-#             f"/out:{output}",
-#         ]
-
-#         cmd += [str(x) for x in obj_files]
-
-#     return cmd
-
-# def find_entry_source(directory: Path) -> tuple[Path, bool]:
-#     stem: str = directory.name
-#     dir_name: str = directory.name.casefold()
-
-#     cpp_src: Path = directory / f"{stem}.cpp"
-#     c_src: Path   = directory / f"{stem}.c"
-
-#     if cpp_src.exists():
-#         return cpp_src, True
-
-#     if c_src.exists():
-#         return c_src, False
-
-#     # Case-insensitive search
-#     c_match: Path | None = None
-
-#     for file in directory.iterdir():
-#         if (file.is_file() and file.stem.casefold() == dir_name):
-#             ext: str  = file.suffix.casefold()
-
-#             if ext == ".cpp":
-#                 return file, True
-            
-#             if ext == ".c":
-#                 c_match = file
-
-#     if c_match is not None:
-#         return c_src, False
-
-#     raise FileNotFoundError(f"Expected '{stem}.cpp' or '{stem}.c' in '{directory}'")
-
-# def build_bof(bof_name: str, env_vars: dict[str, str]):
-#     bof_path: Path
-#     src_file: Path
-#     debugable: bool
-
-#     try:
-#         bof_path = get_bof_path(bof_name)
-#     except FileNotFoundError as e:
-#         # TODO print error?
-#         return
-
-#     try:
-#         src_file, debugable = find_entry_source(bof_path)
-#     except FileNotFoundError as e:
-#         # TODO print
-#         return
-
-
-#     try:
-#         subprocess.run(
-#             CL_CMD + [source_file.name, f"/Fo{output_file}"], 
-#             cwd=proj,
-#             capture_output=True,
-#             check=True,
-#             text=True,
-#             env=env_vars)
-#     except subprocess.CalledProcessError as e:
-#         print(f"\033[91mFAILED\033[0m")
-#         return str(e.output)
-
-
-
-
-
-# def build_project(proj: Path, script_only: bool = False) -> str:
-#     global CNA_SCRIPT_CONTENTS
-
-#     state: Literal['FAIL', 'SUCCESS', 'SKIPPED'] = 'SUCCESS'
-
-#     source_file: Path = proj / f"{proj.name.lower()}.c"
-#     script_file: Path = proj / f"{proj.name.lower()}.cna"
-#     output_file: Path = (BUILD_DIR / f"{proj.name.lower()}.o").resolve()
-
-#     missing_files: list[str] = []
-
-#     script_contents: str = ""
-
-#     err_output: str = ""
-
-#     print(f"Building {proj.name:<30}", end='')
-
-#     if not source_file.is_file():
-#         err_output += f"Cannot find 'c' source file: {source_file.resolve()}\n"
-#         missing_files.append(source_file.name)
-#         state = 'SKIPPED'
-
-#     if not script_file.is_file():
-#         err_output += f"Cannot find 'cna' script file: {script_file.resolve()}\n"
-#         missing_files.append(script_file.name)
-#         state = 'SKIPPED'
-
-#     if state == 'SKIPPED':
-#         print(f"\033[93m{state} \033[90mFiles not found: {missing_files}\033[0m")
-#         return err_output
-
-#     if script_only == False:
-#         # Build the project with cl.exe
-#         try:
-#             vs_env = get_dev_env()
-            
-#             subprocess.run(
-#                 CL_CMD + [source_file.name, f"/Fo{output_file}"], 
-#                 cwd=proj,
-#                 capture_output=True,
-#                 check=True,
-#                 text=True,
-#                 env=vs_env)
-#         except subprocess.CalledProcessError as err:
-#             print(f"\033[91mFAILED\033[0m")
-#             return str(err.output)
-
-
-#     # Get the contents of the script file
-#     script_contents = script_file.read_text(encoding='utf-8')
-
-#     CNA_SCRIPT_CONTENTS += script_contents + "\n\n"
-
-#     print("\033[92mSUCCESS\033[0m" + (" - script only" if script_only else ""))
-
-#     return err_output
-
-# def build_all(scripts_only: bool = False) -> dict[str, str]:
-#     build_errs: dict[str, str] = {}
-
-#     for proj in ROOT_DIR.iterdir():
-#         if not proj.is_dir():
-#             continue
-
-#         err = build_project(proj, scripts_only)
-
-#         if err:
-#             build_errs[proj.name] = err
-
-#     return build_errs
-
 def clean() -> None:
     if BUILD_DIR.exists():
         shutil.rmtree(BUILD_DIR)
@@ -550,8 +287,6 @@ def build_projects(projects: list[str], arch: Arch, config: Config):
     print("Building BOFs")
 
     for p in projects_list:
-        # TODO print building p ... result SUCCESS SKIP FAIL
-        # how to determine skip? maybe not necessary since argparse gathering the directories
         print(f"{p} ... ", end='', flush=True)
 
         if res := builder.build(p, config, arch):
@@ -614,6 +349,7 @@ if __name__ == "__main__":
 
     # TODO -s overrides all building
     # TODO no scripts version?
+    # TODO delete the build folder for the debug objs when done?
 
     if args.projects:
         projects_list = args.projects
@@ -632,54 +368,5 @@ if __name__ == "__main__":
     if BUILD_ERRS:
        print_error_report()
        sys.exit(1)
-
-    sys.exit(0)
-
-    # BUILD_DIR.mkdir(parents=True, exist_ok=True)
-
-    # CNA_SCRIPT_CONTENTS = Path("base.cna").read_text(encoding='utf-8')
-
-
-    # parser.add_argument(
-    #     "projects",
-    #     nargs="*",
-    #     help="Projects to build. If omitted, all projects are built."
-    # )
-
-    # parser.add_argument(
-    #     "-s",
-    #     "--scripts-only",
-    #     action="store_true",
-    #     help="Skip compiling C projects and only combine CNA scripts."
-    # )
-
-    # args = parser.parse_args()
-
-    # if args.projects and args.scripts_only == False:
-    #     projects = [ROOT_DIR / p for p in args.projects]
-        
-    #     for project in projects:
-    #         if not project.exists() or not project.is_dir():
-    #             print(f"Building {project.name:<30}"
-    #                   f"\033[33mSKIPPED\033[0m \033[90m "
-    #                   f"Directory not found: {project.resolve()}\033[0m")
-    #             continue
-
-    #         result = build_project(project)
-
-    #         if result:
-    #             build_errs[project.name] = result
-    # else:
-    #     build_errs = build_all(args.scripts_only)
-
-    # if build_errs:
-    #     print("**********************************************************************\n"
-    #           "*                           Build Errors                             *\n"
-    #           "**********************************************************************")
-    #     for key, value in build_errs.items():
-    #         print(f"> {key}\n{value}")
-    #     sys.exit(1)
-
-    # OUTPUT_SCRIPT_PATH.write_text(CNA_SCRIPT_CONTENTS, encoding='utf-8')
 
     sys.exit(0)
