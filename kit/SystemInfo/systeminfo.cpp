f6d83b486a6fe275aef494900c08ef8be9025fa6
joshua.ziebarth <joshua.ziebarth@cave.lan>
2026-09-08 22:29:48 +0000

    stuff

--- kit/SystemInfo/systeminfo.cpp
+++ kit/SystemInfo/systeminfo.cpp
@@ -1,4 +1,19 @@
+#include <Windows.h>
+#include "base\helpers.h"
 #include "systeminfo.h"
+#include "bofoutput.h"
+
+#ifdef _DEBUG
+	#undef DECLSPEC_IMPORT
+	#define DECLSPEC_IMPORT
+	#include "base\mock.h"
+#endif
+
+extern "C" {
+#include "beacon.h"
+#include "sleepmask.h"
+
+BOF_Buffer buffer = { 0 };
 
 HRESULT WmiGetProperty(IWbemClassObject* pObject, LPCWSTR propName, VARIANT* vt)
 {
@@ -9,17 +24,15 @@ HRESULT WmiGetProperty(IWbemClassObject* pObject, LPCWSTR propName, VARIANT* vt)
         return E_INVALIDARG;
     }
 
-    OLEAUT32$VariantClear(vt);
+    VariantClear(vt);
 
-    return pObject->lpVtbl->Get(pObject, propName, 0, vt, NULL, NULL);
+    return pObject->Get(propName, 0, vt, NULL, NULL);
 }
 
-int go()
+void go(char* args, int len)
 {
     HRESULT hr = S_OK;
     ULONG uReturn = 0;
- 
-    BOF_Buffer buffer = {0};
 
     BOOL bComInitialized = FALSE;
 
@@ -31,11 +44,11 @@ int go()
     IID CLSIDWbemLocator = CLSID_WBEM_LOCATOR;
     IID IIDIWbemLocator  = IIDI_WBEM_LOCATOR;
     
-    BSTR strNetworkResource = OLEAUT32$SysAllocString(L"ROOT\\CIMV2");
-    BSTR strQueryLanguage   = OLEAUT32$SysAllocString(L"WQL");
-    BSTR strOSQuery         = OLEAUT32$SysAllocString(L"SELECT * FROM Win32_OperatingSystem");
-    BSTR strComputerQuery   = OLEAUT32$SysAllocString(L"SELECT * FROM Win32_ComputerSystem");
-    BSTR strQFEngQuery      = OLEAUT32$SysAllocString(L"SELECT * FROM Win32_QuickFixEngineering");
+    BSTR strNetworkResource = SysAllocString(L"ROOT\\CIMV2");
+    BSTR strQueryLanguage   = SysAllocString(L"WQL");
+    BSTR strOSQuery         = SysAllocString(L"SELECT * FROM Win32_OperatingSystem");
+    BSTR strComputerQuery   = SysAllocString(L"SELECT * FROM Win32_ComputerSystem");
+    BSTR strQFEngQuery      = SysAllocString(L"SELECT * FROM Win32_QuickFixEngineering");
 
     VARIANT vtProp;
 
@@ -44,15 +57,15 @@ int go()
         goto cleanup;
     }
 
-    OLEAUT32$VariantInit(&vtProp);
+    VariantInit(&vtProp);
     
-    if (bofstart() == FALSE)
-    {
-        BeaconPrintf(CALLBACK_ERROR, "Not enough memory. Failed to allocate output buffer.\n");
-        goto cleanup;
-    }
+    // if (bofstart() == FALSE)
+    // {
+    //     BeaconPrintf(CALLBACK_ERROR, "Not enough memory. Failed to allocate output buffer.\n");
+    //     goto cleanup;
+    // }
 
-    hr = OLE32$CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
     switch (hr)
     {
         case S_OK:
@@ -66,7 +79,7 @@ int go()
             goto cleanup;
     }
 
-    hr = OLE32$CoInitializeSecurity(
+    hr = CoInitializeSecurity(
         NULL,
         -1,
         NULL,
@@ -84,17 +97,32 @@ int go()
     
     HR_CHECK(
         hr,
-        OLE32$CoCreateInstance(&CLSIDWbemLocator, NULL, CLSCTX_INPROC_SERVER, &IIDIWbemLocator, (void**)&pLoc)
+        CoCreateInstance(
+            CLSIDWbemLocator,
+            NULL,
+            CLSCTX_INPROC_SERVER,
+            IIDIWbemLocator,
+            (void**)&pLoc
+        )
     );
     
     HR_CHECK(
         hr,
-        pLoc->lpVtbl->ConnectServer(pLoc, strNetworkResource, NULL, NULL, NULL, 0, NULL, NULL, &pSvc)
+        pLoc->ConnectServer(
+            strNetworkResource,
+            NULL,
+            NULL,
+            NULL,
+            0,
+            NULL,
+            NULL,
+            &pSvc
+        )
     );
 
     HR_CHECK(
         hr,
-        OLE32$CoSetProxyBlanket(
+        CoSetProxyBlanket(
             (IUnknown*)pSvc,
             RPC_C_AUTHN_WINNT,
             RPC_C_AUTHZ_NONE,
@@ -105,26 +133,25 @@ int go()
             EOAC_NONE)
     );
 
-    char computerName[MAX_COMPUTERNAME_LENGTH + 1] = {0};
-    DWORD size = sizeof(computerName);
+    // char computerName[MAX_COMPUTERNAME_LENGTH + 1] = {0};
+    // DWORD size = sizeof(computerName);
 
-    if (KERNEL32$GetComputerNameA(computerName, &size))
-    {
-        BofPrintf(&buffer, "%s:\n", computerName);
-    }
-    else
-    {
-        BofPrintf(&buffer, "UNKNOWN:\n");
-    }
+    // if (GetComputerNameA(computerName, &size))
+    // {
+    //     BofPrintf(&buffer, "%s:\n", computerName);
+    // }
+    // else
+    // {
+    //     BofPrintf(&buffer, "UNKNOWN:\n");
+    // }
 
-    BofPrintf(&buffer, "  system_info:\n");
+    BofPrintf(&buffer, "system_info:\n");
     
     // =====================================================================================
     // QUERY 1: Win32_OperatingSystem
     // =====================================================================================
     
-    hr = pSvc->lpVtbl->ExecQuery(
-        pSvc,
+    hr = pSvc->ExecQuery(
         strQueryLanguage,
         strOSQuery,
         WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
@@ -134,7 +161,7 @@ int go()
     
     if (SUCCEEDED(hr))
     {
-        hr = OLE32$CoSetProxyBlanket(
+        hr = CoSetProxyBlanket(
             (IUnknown*)pEnumerator,
             RPC_C_AUTHN_WINNT,
             RPC_C_AUTHZ_NONE,
@@ -149,7 +176,7 @@ int go()
         {
             while (pEnumerator)
             {
-                hr = pEnumerator->lpVtbl->Next(pEnumerator, WBEM_INFINITE, 1, &pclsObj, &uReturn);
+                hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
                 if (FAILED(hr) || uReturn == 0)
                 {
                     break;
@@ -157,20 +184,20 @@ int go()
 
                 hr = WmiGetProperty(pclsObj, L"Caption", &vtProp);
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "os_name:",
                     SUCCEEDED(hr) && vtProp.vt == VT_BSTR ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
 
                 hr = WmiGetProperty(pclsObj, L"Version", &vtProp);
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "version:",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
 
                 hr = WmiGetProperty(pclsObj, L"ProductType", &vtProp);
-                PWCHAR _config = FAIL_GET_PROP_STRING;
+                const WCHAR* _config = FAIL_GET_PROP_STRING;
 
                 if (SUCCEEDED(hr))
                 {
@@ -179,15 +206,15 @@ int go()
                         case 1:  _config = L"Standalone Workstation"; break;
                         case 2:  _config = L"Domain Controller";      break;
                         case 3:  _config = L"Server";                 break;
-                        default: _config = L"Unknown";               break;
+                        default: _config = L"Unknown";                break;
                     }
                 }
 
-                BofPrintf(&buffer, "    %-19s%ls\n", "os_configuration:", _config);
+                BofPrintf(&buffer, "  %-19s%ls\n", "os_configuration:", _config);
 
                 hr = WmiGetProperty(pclsObj, L"RegisteredUser", &vtProp);
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "registered_user:",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
@@ -195,29 +222,32 @@ int go()
 
                 hr = WmiGetProperty(pclsObj, L"WindowsDirectory", &vtProp);
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "windows_directory:",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
 
                 hr = WmiGetProperty(pclsObj, L"LastBootUpTime", &vtProp);
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "last_boot_time:",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
 
                 // Try to get locale friendly name
                 hr = WmiGetProperty(pclsObj, L"Locale", &vtProp);
-                LCID _lcid = MSVCRT$wcstoul(vtProp.bstrVal, NULL, 16);
+                
+                LCID _lcid = wcstoul(vtProp.bstrVal, NULL, 16);
+                
                 WCHAR _localeName[LOCALE_NAME_MAX_LENGTH];
-                if (KERNEL32$LCIDToLocaleName(_lcid, _localeName, LOCALE_NAME_MAX_LENGTH, 0))
+
+                if (LCIDToLocaleName(_lcid, _localeName, LOCALE_NAME_MAX_LENGTH, 0))
                 {
-                    BofPrintf(&buffer, "    %-19s%ls\n", "locale:", _localeName);
+                    BofPrintf(&buffer, "  %-19s%ls\n", "locale:", _localeName);
                 }
                 else
                 {
-                    BofPrintf(&buffer, "    %-19s%ls\n", "locale:", vtProp.bstrVal);
+                    BofPrintf(&buffer, "  %-19s%ls\n", "locale:", vtProp.bstrVal);
                 }
                 
                 SAFE_INTERFACE_RELEASE(pclsObj);
@@ -234,8 +264,7 @@ int go()
     // QUERY 2: Win32_ComputerSystem
     // =====================================================================================
     
-    hr = pSvc->lpVtbl->ExecQuery(
-        pSvc,
+    hr = pSvc->ExecQuery(
         strQueryLanguage,
         strComputerQuery,
         WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
@@ -246,7 +275,7 @@ int go()
     if (SUCCEEDED(hr))
     {
         //Applying Proxy Blanket to the second enumerator
-        hr = OLE32$CoSetProxyBlanket(
+        hr = CoSetProxyBlanket(
             (IUnknown*)pEnumerator,
             RPC_C_AUTHN_WINNT,
             RPC_C_AUTHZ_NONE,
@@ -261,7 +290,7 @@ int go()
         {
             while (pEnumerator)
             {
-                hr = pEnumerator->lpVtbl->Next(pEnumerator, WBEM_INFINITE, 1, &pclsObj, &uReturn);
+                hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
                 if (FAILED(hr) || uReturn == 0)
                 {
                     break;
@@ -274,7 +303,7 @@ int go()
                 //     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 // );
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "model:",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
@@ -286,7 +315,7 @@ int go()
                 //     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 // );
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "system_type:",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
@@ -298,7 +327,7 @@ int go()
                 //     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 // );
                 BofPrintf(&buffer,
-                    "    %-19s%ls\n",
+                    "  %-19s%ls\n",
                     "domain:",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
@@ -316,8 +345,7 @@ int go()
     // QUERY 3: Win32_QuickFixEngineering
     // =====================================================================================
     
-    hr = pSvc->lpVtbl->ExecQuery(
-        pSvc,
+    hr = pSvc->ExecQuery(
         strQueryLanguage,
         strQFEngQuery,
         WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
@@ -328,7 +356,7 @@ int go()
     if (SUCCEEDED(hr))
     {
         // Applying Proxy Blanket to the third enumerator
-        hr = OLE32$CoSetProxyBlanket(
+        hr = CoSetProxyBlanket(
             (IUnknown*)pEnumerator,
             RPC_C_AUTHN_WINNT,
             RPC_C_AUTHZ_NONE,
@@ -341,24 +369,20 @@ int go()
         
         if (SUCCEEDED(hr))
         {
-            // internal_printf("    hotfix_ids:\n");
-            BofPrintf(&buffer, "    hotfix_ids:\n");
+            BofPrintf(&buffer, "  hotfix_ids:\n");
             
             while (pEnumerator)
             {
-                hr = pEnumerator->lpVtbl->Next(pEnumerator, WBEM_INFINITE, 1, &pclsObj, &uReturn);
+                hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
                 if (FAILED(hr) || uReturn == 0)
                 {
                     break;
                 }
 
                 hr = WmiGetProperty(pclsObj, L"HotFixID", &vtProp);
-                // internal_printf(
-                //     "      - %ls\n",
-                //     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
-                // );
-                BofPrintf(&buffer,
-                    "      - %ls\n",
+                BofPrintf(
+                    &buffer,
+                    "    - %ls\n",
                     SUCCEEDED(hr) ? vtProp.bstrVal : FAIL_GET_PROP_STRING
                 );
             
@@ -370,7 +394,7 @@ int go()
     }
 
 cleanup:
-    OLEAUT32$VariantClear(&vtProp);
+    VariantClear(&vtProp);
 
     SAFE_INTERFACE_RELEASE(pclsObj);
     SAFE_INTERFACE_RELEASE(pEnumerator);
@@ -385,11 +409,184 @@ cleanup:
     
     if (bComInitialized == TRUE)
     {
-        OLE32$CoUninitialize();
+        CoUninitialize();
     }
     
-    // printoutput(TRUE);
     BofBufferFree(&buffer);
-    
-    return 0;
 }
+} // end extern "C"
+
+// Define a main function for the debug build
+#if defined(_DEBUG) && !defined(_GTEST)
+
+int main(int argc, char* argv[])
+{
+	bof::runMocked<>(go);
+
+	return 0;
+}
+
+// Define unit tests
+#elif defined(_GTEST)
+#include <gtest\gtest.h>
+
+TEST(BofTest, Test1) {
+	std::vector<bof::output::OutputEntry> got =
+		bof::runMocked<>(go);
+	std::vector<bof::output::OutputEntry> expected = {
+		{CALLBACK_OUTPUT, "System Directory: C:\\Windows\\system32"}
+	};
+	// It is possible to compare the OutputEntry vectors, like directly
+	// ASSERT_EQ(expected, got);
+	// However, in this case, we want to compare the output, ignoring the case.
+	ASSERT_EQ(expected.size(), got.size());
+	ASSERT_STRCASEEQ(expected[0].output.c_str(), got[0].output.c_str());
+}
+#endif
+
+/*
+-existing-
+version
+os_configuration
+registered_user
+windows_directory
+last_boot_time
+locale
+model
+system_type
+domain
+hotfix_ids
+
+
+--Win32_OperatingSystem--
+BuildNumber	            Exact Windows build
+OSArchitecture	        64-bit / 32-bit
+InstallDate	            OS installation date
+SerialNumber	        Windows installation/product identifier
+SystemDirectory	        e.g. C:\Windows\System32
+SystemDrive	            e.g. C:
+Manufacturer	        Usually Microsoft Corporation
+Organization	        Registered organization
+ProductType	            Workstation / Domain Controller / Server
+OperatingSystemSKU	    More specific Windows edition/SKU
+OSLanguage	            Numeric Windows language ID
+MUILanguages	        Installed UI languages
+ServicePackMajorVersion	Service pack major
+ServicePackMinorVersion	Service pack minor
+NumberOfProcesses	    Current process count
+NumberOfUsers	        Current logged-on/user count
+TotalVisibleMemorySize	Total usable physical memory
+FreePhysicalMemory	    Available physical memory
+TotalVirtualMemorySize	Total virtual memory
+FreeVirtualMemory	    Free virtual memory
+
+build_number
+os_architecture
+install_date
+system_drive
+system_directory
+product_type
+operating_system_sku
+total_visible_memory
+
+--Win32_ComputerSystem--
+Manufacturer	            Dell, HP, Microsoft, VMware, etc.
+DNSHostName	                Computer hostname
+Name	                    Computer name
+SystemFamily	            Hardware family
+SystemSKUNumber	            System SKU
+TotalPhysicalMemory	        Installed RAM
+NumberOfProcessors	        Physical processors
+NumberOfLogicalProcessors	Logical CPUs
+PartOfDomain	            Domain membership
+DomainRole	                Domain role
+PCSystemType	            Desktop, mobile, workstation, server
+HypervisorPresent	        Whether Windows reports a hypervisor
+UserName	                User currently associated with the computer
+
+computer_name
+hostname
+manufacturer
+model
+system_family
+system_sku
+ram
+cpu_count
+logical_cpu_count
+domain
+domain_role
+part_of_domain
+hypervisor_present
+
+--Win32_Processor--
+Name
+Manufacturer
+NumberOfCores
+NumberOfLogicalProcessors
+MaxClockSpeed
+CurrentClockSpeed
+ProcessorId
+Architecture
+Family
+Model
+Stepping
+L2CacheSize
+L3CacheSize
+VirtualizationFirmwareEnabled
+VMMonitorModeExtensions
+SecondLevelAddressTranslationExtensions
+
+cpu_name
+cpu_manufacturer
+cpu_cores
+cpu_logical_processors
+cpu_max_clock
+cpu_current_clock
+cpu_id
+
+virtualization_firmware_enabled
+vm_monitor_mode_extensions
+slat_supported
+
+-------
+-OS-
+os_name
+os_version
+os_build
+os_architecture
+os_configuration
+registered_user
+windows_directory
+system_directory
+system_drive
+install_date
+last_boot_time
+locale
+
+-Computer-
+computer_name
+hostname
+manufacturer
+model
+system_family
+system_type
+system_sku
+domain
+part_of_domain
+domain_role
+
+-Hardware-
+cpu_name
+cpu_manufacturer
+cpu_cores
+cpu_logical_processors
+cpu_max_clock
+total_memory
+
+-Security / virtualization-
+hypervisor_present
+virtualization_firmware_enabled
+
+-Updates-
+hotfix_ids
+*/
--- kit/SystemInfo/systeminfo.h
+++ kit/SystemInfo/systeminfo.h
@@ -1,31 +1,60 @@
+#pragma once
+#include "base/helpers.h"
+#include <Windows.h>
 #include <wbemidl.h>
-#include "../../common/bof_output.h"
 
+#pragma comment(lib, "ole32.lib")
+//
+// Imports
+//
+extern "C" {
+#ifndef _DEBUG
 //Go
 DECLSPEC_IMPORT HRESULT WINAPI OLE32$CoSetProxyBlanket(IUnknown *pProxy, DWORD dwAuthnSvc, DWORD dwAuthzSvc, OLECHAR *pServerPrincName, DWORD dwAuthnLevel, DWORD dwImpLevel, RPC_AUTH_IDENTITY_HANDLE pAuthInfo, DWORD dwCapabilities);
 DECLSPEC_IMPORT HRESULT WINAPI OLE32$CoInitializeEx(LPVOID pvReserved, DWORD dwCoInit);
-DECLSPEC_IMPORT void WINAPI OLE32$CoUninitialize(void);
+DECLSPEC_IMPORT void    WINAPI OLE32$CoUninitialize(void);
 DECLSPEC_IMPORT HRESULT WINAPI OLE32$CoCreateInstance (REFCLSID rclsid, LPUNKNOWN pUnkOuter, DWORD dwClsContext, REFIID riid, LPVOID *ppv);
 DECLSPEC_IMPORT HRESULT WINAPI OLE32$CoInitializeSecurity(PSECURITY_DESCRIPTOR pSecDesc, LONG cAuthSvc, SOLE_AUTHENTICATION_SERVICE *asAuthSvc, void *pReserved1, DWORD dwAuthnLevel, DWORD dwImpLevel, void *pAuthList, DWORD dwCapabilities, void *pReserved3);
-DECLSPEC_IMPORT void WINAPI OLEAUT32$VariantClear(VARIANTARG *pvarg);
-DECLSPEC_IMPORT void WINAPI OLEAUT32$VariantInit(VARIANTARG *pvarg);
+
+DECLSPEC_IMPORT void    WINAPI OLEAUT32$VariantClear(VARIANTARG *pvarg);
+DECLSPEC_IMPORT void    WINAPI OLEAUT32$VariantInit(VARIANTARG *pvarg);
+
 WINBASEAPI BSTR WINAPI OLEAUT32$SysAllocString(const OLECHAR *);
 WINBASEAPI void WINAPI OLEAUT32$SysFreeString(BSTR);
-WINBASEAPI int __cdecl MSVCRT$printf(const char * _Format,...);
+
+WINBASEAPI int           __cdecl MSVCRT$printf(const char * _Format,...);
 WINBASEAPI unsigned long __cdecl MSVCRT$wcstoul(const wchar_t *nptr, wchar_t **endptr, int base);
+
 WINBASEAPI int WINAPI KERNEL32$LCIDToLocaleName( LCID Locale, LPWSTR lpName, int cchName, DWORD dwFlags);
 
 //bofstart + internal_printf + printoutput
-WINBASEAPI void *__cdecl MSVCRT$calloc(size_t number, size_t size);
-WINBASEAPI int WINAPI MSVCRT$vsnprintf(char* buffer, size_t count, const char* format, va_list arg);
-WINBASEAPI void __cdecl MSVCRT$memset(void *dest, int c, size_t count);
-WINBASEAPI void* WINAPI MSVCRT$memcpy(void* dest, const void* src, size_t count);
-WINBASEAPI HANDLE WINAPI KERNEL32$GetProcessHeap();
-WINBASEAPI LPVOID WINAPI KERNEL32$HeapAlloc(HANDLE hHeap, DWORD dwFlags, SIZE_T dwBytes);
-WINBASEAPI void __cdecl MSVCRT$free(void *memblock);
-WINBASEAPI BOOL WINAPI KERNEL32$HeapFree(HANDLE, DWORD, PVOID);
+// WINBASEAPI void *__cdecl MSVCRT$calloc(size_t number, size_t size);
+// WINBASEAPI int WINAPI MSVCRT$vsnprintf(char* buffer, size_t count, const char* format, va_list arg);
+// WINBASEAPI void __cdecl MSVCRT$memset(void *dest, int c, size_t count);
+// WINBASEAPI void* WINAPI MSVCRT$memcpy(void* dest, const void* src, size_t count);
+// WINBASEAPI HANDLE WINAPI KERNEL32$GetProcessHeap();
+// WINBASEAPI LPVOID WINAPI KERNEL32$HeapAlloc(HANDLE hHeap, DWORD dwFlags, SIZE_T dwBytes);
+// WINBASEAPI void __cdecl MSVCRT$free(void *memblock);
+// WINBASEAPI BOOL WINAPI KERNEL32$HeapFree(HANDLE, DWORD, PVOID);
 
 WINBASEAPI BOOL WINAPI KERNEL32$GetComputerNameA(LPSTR lpBuffer,LPDWORD nSize);
+
+#endif
+}
+#ifndef _DEBUG
+#define CoSetProxyBlanket    OLE32$CoSetProxyBlanket
+#define CoInitializeEx       OLE32$CoInitializeEx
+#define CoUninitialize       OLE32$CoUninitialize
+#define CoCreateInstance     OLE32$CoCreateInstance
+#define CoInitializeSecurity OLE32$CoInitializeSecurity
+#define VariantClear         OLEAUT32$VariantClear
+#define VariantInit          OLEAUT32$VariantInit
+#define SysAllocString       OLEAUT32$SysAllocString
+#define SysFreeString        OLEAUT32$SysFreeString
+#define printf               MSVCRT$printf
+#define wcstoul              MSVCRT$wcstoul
+#define LCIDToLocaleName     KERNEL32$LCIDToLocaleName
+#endif
 
 //
 // Macros
@@ -36,10 +65,10 @@ WINBASEAPI BOOL WINAPI KERNEL32$GetComputerNameA(LPSTR lpBuffer,LPDWORD nSize);
     do { hr = (x); if (FAILED(hr)) { goto lbl; } } while (0);
 
 #define SAFE_INTERFACE_RELEASE(pI) \
-    do { if (pI) { pI->lpVtbl->Release(pI); pI = NULL; } } while (0);
+    do { if (pI) { pI->Release(); pI = NULL; } } while (0);
 
 #define SAFE_SYSFREE_STRING(x) \
-    do { if (x) { OLEAUT32$SysFreeString(x); x = NULL; } } while (0);
+    do { if (x) { SysFreeString(x); x = NULL; } } while (0);
 
 #define FAIL_GET_PROP_STRING L"[Failed to get property]"
 
